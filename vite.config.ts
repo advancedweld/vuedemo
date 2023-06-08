0694d7202f56bdb014717ff260c07ce11d827705
engineMaster <xiangshangzhi@gmail.com>
2023-06-08 23:04:20 +0800

    feat: 支持less

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,27 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import vueJsx from '@vitejs/plugin-vue-jsx'
-// console.log(' @@@@process.env: ', process.env)
+console.log(' @@@@meta ', import.meta.url)
+/*
+ *import.meta.url: 当前文件目录 file:///D:/github/vuedemo/vite.config.ts
+ *fileURLToPath(new URL('./src', import.meta.url)) 拼接当前文件目录为绝对路径
+ */
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        additionalData: `@import "${fileURLToPath(
+          new URL('./src/assets/style/variable.less', import.meta.url)
+        )}";`
+      }
     }
   },
   build: {
