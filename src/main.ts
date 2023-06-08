e24ffd5d6ff95574ee868b225a011f19866ea6e4
xiangshangzhi <xiangshangzhi@163.com>
2023-06-08 08:51:58 +0800

    feat: vue基本语法练习

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,27 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+/* 引入element ui */
+app.use(ElementPlus)
 app.use(router)
 
-app.mount('#app')
+/* https://cn.vuejs.org/guide/essentials/application.html#app-configurations */
+/* 全局错误处理 */
+app.config.errorHandler = (err: unknown) => {
+  console.error('@@@@@@@error', err)
+}
+/* 全局注册组件，使得组件再应用内任何地方都是可用 */
+// app.component('TodoDeleteButton', TodoDeleteButton)
+/* .mount方法会返回根组件实例 */
+
+const root = app.mount('#app')
+console.log('🚀 ~ file: main.ts:15 ~ root:', root)
--- src/views/design/DesignNew.vue
+++ src/views/design/DesignNew.vue
@@ -1,13 +1,71 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+const CASE_MAP = {
+  'V-IF': '条件渲染',
+  'V-FOR': '循环渲染'
+}
+const state = reactive({ count: 0, fruit: ['apple', 'orange'] })
+
+const showCase = ref('V-IF')
+const showH = ref(true)
+function increment() {
+  state.count++
+}
+
+function changeFruit() {
+  const [f0, f1] = state.fruit
+  state.fruit[0] = f1
+  state.fruit[1] = f0
+}
+
+function toggleshowCase() {
+  const index = Math.floor(Math.random() * 2)
+  console.log('@@@@key', index)
+  const key = Object.keys(CASE_MAP)[index]
+  console.log('@@@@key', key)
+  showCase.value = key
+}
+</script>
+
 <template>
-  <div class="about">
-    <h1> hello , design</h1>
+  <div>
+    <div class="about_">
+      <h1>hello , design</h1>
+      <h2>count:{{ state.count }}</h2>
+      <h2>my favoriate food is {{ state.fruit[0] }}</h2>
+    </div>
+    <el-row>
+      <el-button type="primary" @click="state.count--">count--</el-button>
+      <el-button type="primary" @click="increment">count++</el-button>
+      <el-button type="primary" @click="changeFruit">修改水果</el-button>
+    </el-row>
+
+    <el-button type="primary" @click="toggleshowCase">toggleRENDER</el-button>
+    <div v-if="showCase === 'V-IF'">
+      <h1>条件渲染</h1>
+      <div>
+        <h2 v-if="showH">h1 is show</h2>
+        <h2 v-else>h1 is hidden</h2>
+        <el-button @click="() => (showH = !showH)">toggle showH</el-button>
+      </div>
+    </div>
+
+    <div v-if="showCase === 'V-FOR'">
+      <h1>循环渲染</h1>
+      <div>
+        <h2 v-if="showH">h1 is show</h2>
+        <h2 v-else>h1 is hidden</h2>
+        <el-button @click="() => (showH = !showH)">toggle showH</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    /* min-height: 100vh; */
     display: flex;
     align-items: center;
   }
