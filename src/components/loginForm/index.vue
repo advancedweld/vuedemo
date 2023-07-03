87f0146dfa59b9bfe1623291bab3e32feda2bdb2
xiangshangzhi <xiangshangzhi@163.com>
2023-07-03 08:51:06 +0800

    feat: 完善登录

--- src/components/loginForm/index.vue
+++ src/components/loginForm/index.vue
@@ -6,13 +6,17 @@
         <el-form-item label="用户名">
           <el-input v-model="username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
+        <el-form-item label="密 码" width="200px">
           <el-input type="password" v-model="password"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
+      <el-space>
+        <span>{{ `账号：${ACCOUNT}` }}</span>
+        <span>{{ `密码：${PASSWORD}` }}</span>
+      </el-space>
     </el-dialog>
   </div>
 </template>
@@ -22,6 +26,8 @@ import { ref } from 'vue'
 import { ElMessage } from 'element-plus'
 import { useUserProfileStore } from '@/store/userProfile'
 
+const ACCOUNT = 'admin'
+const PASSWORD = '123456'
 const userProfileStore = useUserProfileStore()
 
 const dialogVisible = ref(false)
@@ -37,7 +43,7 @@ const showLoginDialog = () => {
 
 const checkLogin = (userName, pwd) => {
   // 处理登录逻辑
-  if (userName.value === 'admin' && pwd.value === '123456') {
+  if (userName.value === ACCOUNT && pwd.value === PASSWORD) {
     return true
   }
   return false
@@ -47,6 +53,7 @@ const login = () => {
   console.log('@@@@登录', username, password)
   if (checkLogin(username, password)) {
     loginUser()
+    ElMessage.success('登录成功')
     dialogVisible.value = false
   } else {
     ElMessage.error('用户名或密码错误')
